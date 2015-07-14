a83d799f4670c059372a011534db1f5c13f26897
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 14:13:41 -0400

    Finish Projects controller create method.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -19,12 +19,19 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
+    @project.owner_id = current_user.id
     if @project.save
       redirect_to project_url(@project)
     else
       flash.now[:errors] = "Invalid project information"
       render :new
     end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create, :edit, :update, :show] do
-    resources :projects, only: [:index, :new, :create]
+    resources :projects, only: [:index, :new]
   end
   resource :session, only: [:new, :create, :destroy]
-  resources :projects, except: [:index, :new, :create]
+  resources :projects, except: [:index, :new]
 end
