89068428b26ab18247e9777da18e1aea3d38c2b8
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 15:03:14 -0400

    Add tests for Project model.

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :project do
-    
+    title 'The first project'
+    description 'Some things about the project'
+    owner_id 1
   end
 
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid when required attributes are present" do
+    expect(FactoryGirl.build(:project)).to be_valid
+  end
+
+  context 'is invalid' do
+    specify 'when title is blank' do
+      expect(FactoryGirl.build(:project, title: '')).not_to be_valid
+    end
+    specify 'when owner_id is blank' do
+      expect(FactoryGirl.build(:project, owner_id: '')).not_to be_valid
+    end
+  end
 end
