a8368147a057ece3412ed32f2a13758b180e1936
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 10:53:03 -0400

    Add authentication methods to session controller.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,2 +1,24 @@
 class SessionController < ApplicationController
+
+  skip_before_action :require_login!, only: [:new, :create]
+
+  def new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:username], params[:password])
+
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash[:errors] = ["Invalid credentials"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout!
+    redirect_tonew_session_url
+  end
 end
