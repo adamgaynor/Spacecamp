d9077cd18be844f2ecee42e303a3b65f69227424
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 10:42:35 -0400

    Add User model portion of authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ActiveRecord::Base
+
+  attr_reader :password
+
+  validates :email, :password_digest, :session_token, presence: true
+  validates :username, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize: ensure_session_token!
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return unless user
+
+    user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(pasword)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  def reset_token!
+    self.update(session_token: User.generate_session_token)
+    self.session_token
+  end
+
+
+  private
+
+  def ensure_session_token!
+    self.session_token ||= self.generate_session_token
+  end
 end
