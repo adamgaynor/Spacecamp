0ae74c80e9abee0f3721aaf8b3787b9ffb2c0740
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 11:17:07 -0400

    Add methods to users controller for creating new user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+
+  skip_before_action :require_login!, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :fname, :lname, :password)
+  end
 end
