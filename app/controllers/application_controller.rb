1422663f22fbf09dcad3051164c7c01cfaa760a9
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 10:50:21 -0400

    Add application controller methods for user authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,32 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :require_login!
+
+  helper_method :current_user, :logged_in?
+
+  private
+
+  def login!(user)
+    session[:session_token] = user.reset_token!
+  end
+
+  def logout!
+    current_user.reset_token!
+    session[:session_token] = nil
+  end
+
+  def current_user
+    return if session[:session_token].nil?
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login!
+    redirect_to new_session_url unless logged_in?
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ActiveRecord::Base
   validates :username, :session_token, uniqueness: true
   validates :password, length: { minimum: 6 }, allow_nil: true
 
-  after_initialize: ensure_session_token!
+  after_initialize :ensure_session_token!
 
   def self.generate_session_token
     SecureRandom.urlsafe_base64(16)
