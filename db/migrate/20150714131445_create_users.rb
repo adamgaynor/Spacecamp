169a5dd0bf6c0d7dbddfb2b7ee33a61896e0cbd6
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 11:03:51 -0400

    Add first and last names to users, add logout button.

--- db/migrate/20150714131445_create_users.rb
+++ db/migrate/20150714131445_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email, null: false
+      t.string :fname, null: false
+      t.string :lname, null: false
       t.string :password_digest, null: false
       t.string :session_token, null: false
 
