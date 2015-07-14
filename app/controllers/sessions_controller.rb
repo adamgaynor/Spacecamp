597be63a1803857c07c4ae4c1c2a579476adf1f4
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 11:46:19 -0400

    Users can log in and out.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       login!(@user)
       redirect_to user_url(@user)
     else
-      flash[:errors] = ["Invalid credentials"]
+      flash.now[:errors] = ["Invalid credentials"]
       render :new
     end
   end
