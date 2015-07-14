8a98fc1aaea343ae24f4e6a4d54d5858390959cc
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 14:59:44 -0400

    Add tests for User model.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
+require 'BCrypt'
+
 FactoryGirl.define do
   factory :user do
-    
+    email 'john_smith@gmail.com'
+    fname 'John'
+    lname 'Smith'
+    password_digest BCrypt::Password.create("aaaaaa")
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid when required attributes are present" do
+    expect(FactoryGirl.build(:user)).to be_valid
+  end
+
+  context 'is invalid' do
+    specify 'when email is blank' do
+      expect(FactoryGirl.build(:user, email: '')).not_to be_valid
+    end
+    specify 'when fname is blank' do
+      expect(FactoryGirl.build(:user, fname: '')).not_to be_valid
+    end
+    specify 'when lname is blank' do
+      expect(FactoryGirl.build(:user, lname: '')).not_to be_valid
+    end
+  end
 end
