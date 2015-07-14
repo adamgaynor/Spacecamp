d54029df51e529ea437ef718a03672a185b9f00f
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-14 13:57:34 -0400

    Add rails routes for projects.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create, :edit, :update, :show]
+  resources :users, only: [:new, :create, :edit, :update, :show] do
+    resources :projects, only: [:index, :new, :create]
+  end
   resource :session, only: [:new, :create, :destroy]
+  resources :projects, except: [:index, :new, :create]
 end
