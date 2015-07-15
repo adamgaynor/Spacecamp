4ddc0f36503c4289aefc5ee98698d0bd95afe8b2
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-15 14:03:01 -0400

    Write project show view initialize.

--- app/assets/javascripts/views/project_show.js
+++ app/assets/javascripts/views/project_show.js
@@ -1,3 +1,16 @@
 SpaceCamp.Views.ProjectShow = Backbone.View.extend({
-  template: JST['projects/show']
+  template: JST['projects/show'],
+
+  tagName: 'section',
+
+  initialize: function (options) {
+    this.project = options.project;
+    this.listenTo(this.project, "sync", this.render);
+    this.$el.attr("class", "project-show group");
+
+  },
+
+  render: function () {
+    var content;
+  }
 });
