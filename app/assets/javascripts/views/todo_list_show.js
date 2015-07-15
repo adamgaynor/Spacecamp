854e624c33b806ae58d55c014e2822338de88c5c
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-15 14:31:38 -0400

    Write ToDoList template and view.

--- app/assets/javascripts/views/todo_list_show.js
+++ app/assets/javascripts/views/todo_list_show.js
@@ -1,3 +1,13 @@
 SpaceCamp.Views.ToDoListShow = Backbone.View.extend({
-  template: JST['todo_lists/show']
+  template: JST['todo_lists/show'],
+
+  initialize: function (options) {
+    this.list = options.list
+  },
+
+  render: function () {
+    var content = this.template({ list: this.list });
+    this.$el.html(content);
+    return this;
+  }
 });
