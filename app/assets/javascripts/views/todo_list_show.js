76b2de3784f062af04cefd472d91d562fa14295b
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-16 12:02:40 -0400

    Write methods for todo list show composite view.

--- app/assets/javascripts/views/todo_list_show.js
+++ app/assets/javascripts/views/todo_list_show.js
@@ -10,7 +10,36 @@ SpaceCamp.Views.ToDoListShow = Backbone.CompositeView.extend({
   render: function () {
     var content = this.template({ list: this.model });
     this.$el.html(content);
-    
+
+    this.removeAllItems();
+    this.toDoItems.each(this.addToDoItem.bind(this));
+    this.addCreateToDoItem();
+    this.attachSubviews();
+
     return this;
+  },
+
+  removeAllItems: function () {
+    var subviews = this.subviews('.todo-items');
+    var views = subviews.clone();
+    views.forEach(this.removeSubview.bind(this, '.todo-items'));
+  },
+
+  addToDoItem: function (toDoItem) {
+    var itemView = new SpaceCamp.Views.ToDoItemShow({
+      model: toDoItem,
+      collection: this.collection
+    });
+    this.addSubview(".todo-items", itemView);
+  },
+
+  addCreateToDoItem: function () {
+    var toDoItem = new SpaceCamp.Models.ToDoItem();
+    var createToDoItemForm = new SpaceCamp.Views.ToDoItemForm({
+      toDoList: this.model,
+      collection: this.collection,
+      model: toDoItem
+    });
+    this.addSubview(".todo-lists", createToDoItemForm);
   }
 });
