0a1bebdc9af9cc040c40088e36eca47cf50b2a24
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-17 14:38:44 -0400

    Write db migration for comments.

--- db/migrate/20150717183558_create_comments.rb
+++ db/migrate/20150717183558_create_comments.rb
@@ -1,8 +1,13 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
+      t.integer :author_id, null: false
+      t.integer :discussion_id, null: false
+      t.text :content, null: false
 
       t.timestamps null: false
     end
+    add_index :comments, :author_id
+    add_index :comments, :discussion_id
   end
 end
