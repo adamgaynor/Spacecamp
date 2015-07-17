35f88859de312904903cf389a99fe0dd8f7bfb94
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-17 16:56:00 -0400

    Add render functions for comment form and show views.

--- app/assets/javascripts/views/comment_form.js
+++ app/assets/javascripts/views/comment_form.js
@@ -1,3 +1,17 @@
 SpaceCamp.Views.CommentForm = Backbone.View.extend({
-	template: JST['comments/form']
+	template: JST['comments/form'],
+
+	initialize: function (options) {
+		this.discussion = options.discussion;
+		//this.listenTo(this.model, "sync", this.render);
+	},
+
+	render: function () {
+		var content = this.template({
+			comment: this.model
+		});
+		this.$el.html(content);
+
+		return this;
+	}
 });
--- app/assets/javascripts/views/comment_show.js
+++ app/assets/javascripts/views/comment_show.js
@@ -1,3 +1,13 @@
 SpaceCamp.Views.CommentShow = Backbone.View.extend({
-	template: JST['comments/show']
+	template: JST['comments/show'],
+
+	render: function () {
+		var content = this.template({
+			comment: this.model,
+			discussion: this.collection
+		});
+		this.$el.html(content);
+
+		return this;
+	}
 });
