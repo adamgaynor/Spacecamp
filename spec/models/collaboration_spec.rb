9d22c65e01d18c755e13b4d9ab1024715c4bbbea
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-17 23:53:45 -0400

    Add associaion tests for user and collaboration.

--- spec/models/collaboration_spec.rb
+++ spec/models/collaboration_spec.rb
@@ -3,3 +3,23 @@ require 'rails_helper'
 RSpec.describe Collaboration, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+RSpec.describe Collaboration, type: :model do
+  it "is valid when required attributes are present" do
+    expect(FactoryGirl.build(:collaboration)).to be_valid
+  end
+
+  context 'is invalid' do
+    specify 'when project_id is blank' do
+      expect(FactoryGirl.build(:collaboration, project_id: '')).not_to be_valid
+    end
+    specify 'when user_id is blank' do
+      expect(FactoryGirl.build(:collaboration, user_id: '')).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it { should belong_to(:project) }
+    it { should belong_to(:user) }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,5 +44,7 @@ RSpec.describe User, type: :model do
     it { should have_many(:to_do_lists) }
     it { should have_many(:to_do_items) }
     it { should have_many(:authored_comments) }
+    it { should have_many(:collaborations) }
+    it { should have_many(:collaborated_projects) }
   end
 end
