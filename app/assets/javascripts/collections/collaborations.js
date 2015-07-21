15c3c5db999bc32560ee78a6f91a683a983ed522
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-21 14:50:01 -0400

    Remove getOrFetch from collaborations collection.

--- app/assets/javascripts/collections/collaborations.js
+++ app/assets/javascripts/collections/collaborations.js
@@ -1,19 +1,3 @@
 SpaceCamp.Collections.Collaborations = Backbone.Collection.extend({
-  model: SpaceCamp.Models.Collaboration,
-
-  // getOrFetch: function (id) {
-  //   var collaborations = this;
-  //   var collaboration = this.get(id);
-  //   if (collaboration) {
-  //     collaboration.fetch();
-  //   } else {
-  //     collaboration = new SpaceCamp.Models.Collaboration({ id: id });
-  //     collaboration.fetch({
-  //       success: function () {
-  //         collaborations.add(collaboration);
-  //       }
-  //     });
-  //   }
-  //   return collaboration;
-  // }
+  model: SpaceCamp.Models.Collaboration
 });
