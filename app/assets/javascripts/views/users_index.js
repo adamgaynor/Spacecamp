d683fa724898b3055f7b19bd50fb1cedcd25a3fc
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-21 11:18:58 -0400

    Add remove buttons to users index.

--- app/assets/javascripts/views/users_index.js
+++ app/assets/javascripts/views/users_index.js
@@ -6,14 +6,19 @@ SpaceCamp.Views.UsersIndex = Backbone.View.extend({
   initialize: function (options) {
     this.project = options.project;
     this.users = options.users;
+    this.collaborators = this.project.collaborators();
     this.$el.attr("class", "users-list");
     this.listenTo(this.users, "sync", this.render);
+    this.listenTo(this.project, "sync", this.render);
+
   },
 
   render: function () {
-    console.log(this.users);
+    collaborators = this.collaborators;
+    users = this.users;
     var content = this.template({
-      users: this.users
+      users: this.users,
+      collaborators: collaborators
     });
     this.$el.html(content);
 
