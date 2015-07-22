85df296b11dbdbb97200ea8d95ab69b9171a698b
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-21 20:53:39 -0400

    Put secrets.yml back into the project.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  #config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
   config.serve_static_assets = true
   config.assets.compile = true
