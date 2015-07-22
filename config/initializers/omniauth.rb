292a8ec7c8aade0c5e6ef4c78f9834b43a38cf57
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-22 15:32:04 -0400

    Change root for omniauth to correct path.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,5 @@
 # https://www.twilio.com/blog/2014/09/gmail-api-oauth-rails.html
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
-  scope: ['email',
-    'https://www.googleapis.com/auth/gmail.modify'],
-    access_type: 'offline'}
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
 end
