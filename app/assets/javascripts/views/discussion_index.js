6ddbfcb3839d2fa4fa99346947c69c4c9ce0e2f8
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-23 00:02:10 -0400

    Write discussion index view initialize method.

--- app/assets/javascripts/views/discussion_index.js
+++ app/assets/javascripts/views/discussion_index.js
@@ -1,3 +1,11 @@
 SpaceCamp.Views.DiscussionsIndex = Backbone.CompositeView.extend({
-  template: JST['discussions/index']
+  template: JST['discussions/index'],
+
+  tagName: 'table',
+
+  initialize: function (options) {
+    this.discussions = options.discussions;
+    this.project = options.project;
+    this.$el.attr("class", "discussions");
+  }
 });
