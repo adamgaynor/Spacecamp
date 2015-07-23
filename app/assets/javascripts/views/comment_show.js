c421feae9cf4ed858209383b9d468d4db4e947a8
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-23 11:07:18 -0400

    Use mixin for comment show view and remove redundant methods.

--- app/assets/javascripts/views/comment_show.js
+++ app/assets/javascripts/views/comment_show.js
@@ -1,56 +1,24 @@
-SpaceCamp.Views.CommentShow = Backbone.View.extend({
-	template: JST['comments/show'],
+SpaceCamp.Views.CommentShow = Backbone.View.extend(
+	_.extend({}, SpaceCamp.Mixins.ElapsedTime, {
+		template: JST['comments/show'],
 
-	tagName: 'article',
+		tagName: 'article',
 
-	initialize: function (options) {
-		this.$el.attr("class", "comment-article");
-		this.listenTo(this.collection, "add", this.render);
-	},
+		initialize: function (options) {
+			this.$el.attr("class", "comment-article");
+			this.listenTo(this.collection, "add", this.render);
+		},
 
-	render: function () {
-		var updated = new Date(this.model.get("updated_at"));
-		var timeSinceUpdate = this.getElapsedTime(updated);
-		var content = this.template({
-			comment: this.model,
-			time: timeSinceUpdate
-		});
-		this.$el.html(content);
+		render: function () {
+			var updated = new Date(this.model.get("updated_at"));
+			var timeSinceUpdate = this.getElapsedTime(updated);
+			var content = this.template({
+				comment: this.model,
+				time: timeSinceUpdate
+			});
+			this.$el.html(content);
 
-		return this;
-	},
-
-	// processes the last date the comment was updated, and gives the
-	// time ago in a readable format
-	getElapsedTime: function (date) {
-    var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-    var delta = parseInt((relative_to.getTime() - date) / 1000);
-    //delta = delta + (relative_to.getTimezoneOffset() * 60);
-
-    if (delta < 60) { return 'less than a minute ago'; }
-    else if (delta < 120) { return 'a minute ago'; }
-    else if (delta < (60 * 60)) { return (parseInt(delta / 60)).toString() + ' minutes ago'; }
-    else if (delta < (120 * 60)) { return 'an hour ago'; }
-    else if (delta < (24 * 60 * 60)) { return '' + (parseInt(delta / 3600)).toString() + ' hours ago'; }
-    else if (delta < (2 * 24 * 60 * 60)) { return '1 day ago'; }
-		else if (delta < (365 * 24 * 60 * 60)) {
-			return this.getMonthAndDay(date);
-		} else {
-			return this.getMonthAndDay(date, true);
+			return this;
 		}
-    // else {
-		// 	return (parseInt(delta / 86400)).toString() + ' days ago';
-		// }
-	},
-
-	getMonthAndDay: function (date, includeYear) {
-		var dateString = date.toDateString();
-		dateString = dateString.split(" ");
-		if (includeYear) {
-			dateString = dateString.splice(1,3);
-		} else {
-			dateString = dateString.splice(1,2);
-		}
-		return dateString.join(" ");
-	}
-});
+	})
+);
