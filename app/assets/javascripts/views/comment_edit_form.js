9a10e7754c5e7fb424d2f03802b127597d67dd91
Adam Gaynor <adamgaynor71017@gmail.com>
2015-07-27 15:19:44 -0400

    Add comment edit form subview to every comment.

--- app/assets/javascripts/views/comment_edit_form.js
+++ app/assets/javascripts/views/comment_edit_form.js
@@ -1,3 +1,29 @@
 SpaceCamp.Views.CommentEditForm = Backbone.View.extend({
-  
+  template: JST['comments/form'],
+
+  tagName: "form",
+
+  events: {
+    'click button': 'submit'
+  },
+
+  className: 'comment-form',
+
+
+  initialize: function (options) {
+		this.discussion = options.discussion;
+	},
+
+	render: function () {
+		var content = this.template({
+			comment: this.model
+		});
+		this.$el.html(content);
+
+		return this;
+	},
+
+  submit: function (event) {
+    event.preventDefault();
+  }
 });
--- app/assets/javascripts/views/comment_show.js
+++ app/assets/javascripts/views/comment_show.js
@@ -1,16 +1,16 @@
-SpaceCamp.Views.CommentShow = Backbone.View.extend(
+SpaceCamp.Views.CommentShow = Backbone.CompositeView.extend(
 	_.extend({}, SpaceCamp.Mixins.ElapsedTime, {
 		template: JST['comments/show'],
 
 		tagName: 'article',
 
 		events: {
-			'click .edit-button': 'submit'
+			'click .edit-button': 'showEditForm'
 		},
 
 		initialize: function (options) {
 			this.$el.attr("class", "comment-article");
-			this.listenTo(this.collection, "add", this.render);
+			this.listenTo(this.collection, "add change", this.render);
 		},
 
 		render: function () {
@@ -22,11 +22,34 @@ SpaceCamp.Views.CommentShow = Backbone.View.extend(
 			});
 			this.$el.html(content);
 
+			this.removeAllForms();
+	    this.addEditComment();
+	    this.attachSubviews();
+
 			return this;
 		},
 
-		submit: function (event) {
-			event.preventDefault();
-		}
+		removeAllForms: function () {
+	    var subviews = this.subviews('.comment-edit-form');
+	    var views = subviews.clone();
+	    views.forEach(this.removeSubview.bind(this, '.comment-edit-form'));
+	  },
+
+	  addEditComment: function () {
+	    var comment = this.model;
+	    var createCommentEditForm = new SpaceCamp.Views.CommentEditForm({
+	      model: comment,
+	      collection: this.collection
+	    });
+	    this.addSubview(".comment-edit-form", createCommentEditForm);
+	  },
+
+	  showEditForm: function (event) {
+	    event.preventDefault();
+			debugger
+	    var $target = $(event.delegateTarget);
+	    var $form = $target.find(".comment-edit-form");
+	    $form.addClass("show");
+	  }
 	})
 );
